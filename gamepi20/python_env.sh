9b1c2dfd24b8c0427437944f0026349573578623
johntelforduk <jtelford1@gmail.com>
2021-09-18 16:11:44 +0100

    Script to prepare Python.

---
+++ gamepi20/python_env.sh
@@ -0,0 +1,8 @@
+python3 -m venv ~/venv/space
+source ~/venv/space/bin/activate
+
+sudo apt-get install libsdl-ttf2.0-0
+sudo apt-get install libsdl-mixer1.2
+sudo apt-get install python3-sdl2
+
+pip install pygame
